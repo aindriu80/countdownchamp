0f6e54d1e9903b37f862f82116eeb7d83e4f0049
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-06-29 21:34:26 +0100

    Hooking with LifeCycle Methods

--- countdownchamp/src/Clock.jsx
+++ countdownchamp/src/Clock.jsx
@@ -13,22 +13,32 @@ class Clock extends Component {
         // console.log('this.props', this.props);        
     }
 
+    componentWillMount() {
+        this.getTimeUntil(this.props.deadline);
+    }
+
+    componentDidMount() {
+        setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
+    }
+
     getTimeUntil(deadline) {
         const time = Date.parse(deadline) - Date.parse(new Date());
-        console.log('time', time);
+        // console.log('time', time);
         const seconds = Math.floor((time / 1000) % 60);
         const minutes = Math.floor((time / 1000 / 60) % 60);
         const hours = Math.floor(time / (1000 * 60 * 60) % 24);
         const days = Math.floor(time / (1000 * 60 * 60 * 24));
 
-        console.log('seconds', seconds, 'minutes', minutes, 'hours', hours, 'days', days);
-        //  this.setState({ days: days });
+        // console.log('seconds', seconds, 'minutes', minutes, 'hours', hours, 'days', days);
+
+        // this.setState({ days: days, hours: hours, minutes: minutes, seconds: seconds });
+        // ES6 shortens the above to this
+        this.setState({ days, hours, minutes, seconds });
+
 
 
     }
     render() {
-        this.getTimeUntil(this.props.deadline);
-
         return (
             <div>
                 <div className="Clock-days">{this.state.days} days</div>
